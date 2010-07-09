1818a42d6c895e525c1513725b1772f6eefa58c7
Lenni <leonard.ehrenfried@web.de>
2010-07-09 23:46:58 +0200

    renaming files

--- app/models/filename.rb
+++ app/models/filename.rb
@@ -2,7 +2,7 @@ module Filename
   # Requires an instance variable @value
   def file_name
     dir=Rails.root.to_s+"/app/templates/"+self.class.name.downcase+"s/"
-    file_name=name.downcase.gsub(" ","_")+".erb"
+    file_name=name.downcase.gsub(" ","_")+".html.erb"
     full_path=dir+file_name
     
     matches=Dir.entries(dir).detect {|f| f==file_name}
