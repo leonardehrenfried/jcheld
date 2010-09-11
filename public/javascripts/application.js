dac9bc2ec15f52300a9690c63ef221025bdea1ae
Lenni <leonard.ehrenfried@web.de>
2010-09-11 15:17:41 +0200

    animated header

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+  var headerImgs=$("header img");
+  if(headerImgs.length>1){
+    headerImgs.eq(1).animate({opacity:0}, 8000);
+  }
+});
