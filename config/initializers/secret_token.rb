69bc4e9f35584ede374ef01eaa9f70622dcac4a4
Lenni <leonard.ehrenfried@web.de>
2010-09-25 00:13:29 +0200

    last stragglers checked in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jcheld3::Application.config.secret_token = '4e5c49ea8f461fc7361ac292bfda6ab82175fd690da35b874e02f81d79e2040ab19a28023e39031f944f34a7a4fa343da6c3a5f0d0930a08ebc18a4bb5ec694a'
