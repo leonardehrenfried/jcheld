49442643b0de61b0d9152fa700c5cddbb600f092
Lenni <leonard.ehrenfried@web.de>
2010-10-12 21:47:48 +0200

    better photo attachment handling

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,12 +3,13 @@ class Photo < ActiveRecord::Base
   has_attachment :content_type => :image, 
                  :storage => :file_system, 
                  :max_size => 2000.kilobytes,
-                 :resize_to => '500x500>',
+                 :resize_to => '950x950>',
                  :thumbnails => { :thumb => '215x215>',
-                                  :full_width => '500x950'
+                                  :square_thumb => 'c200x200',
                                 }
 
   validates_as_attachment
 
   belongs_to :article
+
 end
