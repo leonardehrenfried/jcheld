b56885d161b64d10875d1e7ce5547b94fe6f06ce
Lenni <leonard.ehrenfried@web.de>
2010-10-14 23:05:11 +0200

    high quality for photo gallery & new animated header

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,10 +2,11 @@ class Photo < ActiveRecord::Base
   
   has_attachment :content_type => :image, 
                  :storage => :file_system, 
-                 :max_size => 2000.kilobytes,
-                 :resize_to => '950x950>',
+                 :max_size => 3000.kilobytes,
+                 :resize_to => '3000x3000>',
                  :thumbnails => { :thumb => '215x215>',
                                   :square_thumb => 'c200x200',
+                                  :page_width => '900x900',
                                 }
 
   validates_as_attachment
