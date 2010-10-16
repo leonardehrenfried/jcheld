3e036aa9a04328b46dfda1ba31b9f9db8f888eaa
Lenni <leonard.ehrenfried@web.de>
2010-10-16 19:06:11 +0200

    added fancybox

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
   if(headerImgs.length>1){
     headerImgs.eq(1).animate({opacity:0}, 8000);
   }
+
+  $(".fancybox").fancybox({
+    titleShow:false,
+    overlayOpacity:0.8
+  });
 });
