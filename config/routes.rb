6d67bdf691198cad503d7a91b1d31f96f1b193d6
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2012-01-02 23:06:59 +0100

    Update to newest rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Jcheld3::Application.routes.draw do
-  devise_for :users
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
+  devise_for :users
+  
 
   root :controller => "pages", :action => "show", :id => 4 
   resources :pages, :articles, :layouts, :headers do  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110102122909) do
+ActiveRecord::Schema.define(:version => 20120102220506) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -83,6 +83,19 @@ ActiveRecord::Schema.define(:version => 20110102122909) do
     t.integer "height"
     t.integer "article_id"
   end
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "tiles", :force => true do |t|
     t.date     "creation_date"
