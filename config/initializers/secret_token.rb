737aba1719544d54fbf9d6af2ce6888f4cce682f
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2012-01-04 00:10:37 +0100

    Update to newest Rails config

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Jcheld3::Application.config.secret_token = '4e5c49ea8f461fc7361ac292bfda6ab82175fd690da35b874e02f81d79e2040ab19a28023e39031f944f34a7a4fa343da6c3a5f0d0930a08ebc18a4bb5ec694a'
+Jcheld3::Application.config.secret_token = 'b14803ccf164fd14a28e9bf52fbb8eccedc2c02a888f40e99957ae71ba274b8c534a1dd91e9db6610ef4ab5914db7f902936deb6c556bf2e7f698d5db4ad94c7'
