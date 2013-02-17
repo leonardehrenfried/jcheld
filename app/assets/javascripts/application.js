f6414442422db13a2932d9e05c1f757df5baa3ec
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2013-02-17 13:18:10 +0100

    Change last header image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,6 @@
 //= require jquery.fancybox-1.3.1.pack
 
 $(document).ready(function(){
-  var headerImgs=$("header img");
-  if(headerImgs.length>1){
-    headerImgs.eq(1).animate({opacity:0}, 8000);
-  }
-
   $(".fancybox").fancybox({
     titleShow:false,
     overlayOpacity:0.8
